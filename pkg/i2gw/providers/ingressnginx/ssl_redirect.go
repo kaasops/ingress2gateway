23dc5d9881f663ca5030cdc2df840ccd8b323f82
Denis Khachyan <khachyanda@gmail.com>
2024-06-01 23:37:34 +0300

    rename func

--- pkg/i2gw/providers/ingressnginx/ssl_redirect.go
+++ pkg/i2gw/providers/ingressnginx/ssl_redirect.go
@@ -20,7 +20,7 @@ func sslRedirectFeature(ingresses []networkingv1.Ingress, gatewayResources *i2gw
 	ruleGroups := common.GetRuleGroups(ingresses)
 	for _, rg := range ruleGroups {
 		for _, rule := range rg.Rules {
-			if needsRedirectToHttps(rule.Ingress) {
+			if needSslRedirect(rule.Ingress) {
 				if rule.Ingress.Spec.Rules == nil {
 					continue
 				}
@@ -45,7 +45,7 @@ func sslRedirectFeature(ingresses []networkingv1.Ingress, gatewayResources *i2gw
 	return nil
 }
 
-func needsRedirectToHttps(ingress networkingv1.Ingress) bool {
+func needSslRedirect(ingress networkingv1.Ingress) bool {
 	v, ok := ingress.Annotations["nginx.ingress.kubernetes.io/force-ssl-redirect"]
 	if ok && strings.ToLower(v) == "true" {
 		return true
