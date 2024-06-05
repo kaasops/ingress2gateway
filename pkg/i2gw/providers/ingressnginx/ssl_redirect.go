b99a1a1b3e3c44a4b7fac7339fe9544be5169ff0
Denis Khachyan <khachyanda@gmail.com>
2024-06-05 16:20:50 +0300

    fix redirect

--- pkg/i2gw/providers/ingressnginx/ssl_redirect.go
+++ pkg/i2gw/providers/ingressnginx/ssl_redirect.go
@@ -1,11 +1,13 @@
 package ingressnginx
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw"
 	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw/providers/common"
 	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/utils/ptr"
@@ -30,8 +32,38 @@ func sslRedirectFeature(ingresses []networkingv1.Ingress, gatewayResources *i2gw
 				if !ok {
 					errs = append(errs, field.NotFound(field.NewPath("HTTPRoute"), key))
 				}
-				appendRedirectFilter(&httpRoute)
-				gatewayResources.HTTPRoutes[key] = httpRoute
+
+				redirectKey := types.NamespacedName{Namespace: rule.Ingress.Namespace, Name: fmt.Sprintf("%s-redirect", common.RouteName(rg.Name, rg.Host))}
+				if _, ok := gatewayResources.HTTPRoutes[redirectKey]; ok {
+					continue
+				}
+				redirectRoute := gatewayv1.HTTPRoute{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: rule.Ingress.Namespace,
+						Name:      redirectKey.Name,
+					},
+					Spec: gatewayv1.HTTPRouteSpec{
+						Hostnames: []gatewayv1.Hostname{gatewayv1.Hostname(rg.Host)},
+						CommonRouteSpec: gatewayv1.CommonRouteSpec{
+							ParentRefs: httpRoute.Spec.ParentRefs,
+						},
+						Rules: []gatewayv1.HTTPRouteRule{
+							{
+								Filters: []gatewayv1.HTTPRouteFilter{
+									{
+										Type: gatewayv1.HTTPRouteFilterRequestRedirect,
+										RequestRedirect: &gatewayv1.HTTPRequestRedirectFilter{
+											Scheme:     ptr.To(httpsRedirectScheme),
+											StatusCode: ptr.To(301),
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+				redirectRoute.SetGroupVersionKind(common.HTTPRouteGVK)
+				gatewayResources.HTTPRoutes[redirectKey] = redirectRoute
 			}
 		}
 	}
@@ -51,18 +83,4 @@ func requireSSLRedirect(ingress networkingv1.Ingress) bool {
 		return true
 	}
 	return false
-}
-
-func appendRedirectFilter(httpRoute *gatewayv1.HTTPRoute) {
-	httpRoute.Spec.Rules = append(httpRoute.Spec.Rules, gatewayv1.HTTPRouteRule{
-		Filters: []gatewayv1.HTTPRouteFilter{
-			{
-				Type: gatewayv1.HTTPRouteFilterRequestRedirect,
-				RequestRedirect: &gatewayv1.HTTPRequestRedirectFilter{
-					Scheme:     ptr.To(httpsRedirectScheme),
-					StatusCode: ptr.To(301),
-				},
-			},
-		},
-	})
 }
