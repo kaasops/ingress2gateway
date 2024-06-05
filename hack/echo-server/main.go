cbd682bed89b8a3718ec01dc79a5c22dfc67dca5
Denis Khachyan <khachyanda@gmail.com>
2024-06-05 11:02:55 +0300

    added echo server

--- hack/echo-server/main.go
+++ hack/echo-server/main.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
 )
+
+var (
+	DefaultEndpoint = []string{"/"}
+)
+
+type Config struct {
+	Endpoints []string `yaml:"endpoints"`
+}
 
 func echo(c *gin.Context) {
 	c.JSON(200, gin.H{
@@ -11,17 +24,34 @@ func echo(c *gin.Context) {
 }
 
 func main() {
-	endpoints := []string{
-		"/ping",
-		"/hello",
-		"/goodbye",
-		"/endpoint1",
-		"/endpoint2",
-		"/endpoint3",
+	var configPath string
+	var Port string
+
+	flag.StringVar(&configPath, "config", "", "Path to config file")
+	flag.StringVar(&Port, "port", "8080", "Port to listen on")
+	flag.Parse()
+
+	config := Config{}
+	if configPath != "" {
+		content, err := os.ReadFile(configPath)
+		if err != nil {
+			panic(err)
+		}
+
+		err = yaml.Unmarshal(content, &config)
+		if err != nil {
+			panic(err)
+		}
 	}
+
+	if len(config.Endpoints) == 0 {
+		config.Endpoints = DefaultEndpoint
+	}
+
 	router := gin.Default()
-	for _, endpoint := range endpoints {
+	for _, endpoint := range config.Endpoints {
 		router.GET(endpoint, echo)
 	}
-	router.Run(":8080")
+
+	router.Run(fmt.Sprintf(":%s", Port))
 }
